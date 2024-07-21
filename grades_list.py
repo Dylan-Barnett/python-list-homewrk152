f599c039b7c173e4d4ffdc88adb196152481dace
Dylan-Barnett <D534992010@gmail.com>
2024-07-21 03:03:36 -0500

    initial commit

---
+++ grades_list.py
@@ -0,0 +1,13 @@
+grades = [85,90,78,88,76,95,89,80,72,93]
+grades.sort()
+print(grades)
+#sort the grades in order
+
+# 2 calculate the average list by div the sum of all the graDES BY LENGHT OF LISTS
+
+print(len(grades))
+sum = (72 + 76 + 78 + 80 + 85 + 88 + 89 + 90 + 93 + 95)
+print(sum)
+
+average = (846 / 10)
+print(average)
